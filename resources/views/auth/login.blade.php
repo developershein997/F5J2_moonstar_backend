2adcbc43d96c42300e85f12f6aa9c809f7bf2e1b
AMK1215 <aungmyokhaing1215@gmail.com>
2025-08-01 11:08:43 +0630

    staging update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BK999 | Dashboard</title>
+    <title>MoonStar | Dashboard</title>
 
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
